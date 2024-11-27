d62f72d0f641b1efabbd725f8462e83d2a8317cc
Márcio alves Ramos <marcioalvesramos90@hotmail.com>
2024-11-26 23:09:35 -0300

    feat: start drawing something

--- app.js
+++ app.js
@@ -1,2 +1,39 @@
-let h1 = document.querySelector('h1');
-h1.textContent = 'calm down we are under construction!';
+let canvas = document.getElementById("board");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+let ctx = canvas.getContext("2d");
+
+let isDrawing = false;
+let mousePos = { x: null, y: null };
+
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("touchend", end, false);
+canvas.addEventListener("mouseup", end, false);
+canvas.addEventListener("mousemove", draw, false);
+canvas.addEventListener("touchmove", draw, false);
+
+function start(event) {
+    isDrawing = true;
+    ctx.beginPath();
+    ctx.moveTo(event.clientX, event.clientY);
+    event.preventDefault();
+}
+
+function end(event) {
+    isDrawing = false;
+}
+
+function draw(event) {
+    let pos = mousePos;
+    if (isDrawing) {
+        ctx.lineTo(event.clientX, event.clientY);
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 2;
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.stroke();
+    }
+}
